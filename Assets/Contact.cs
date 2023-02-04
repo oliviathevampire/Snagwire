46a3df05220f9bc64edc90df36924a3c288f4f33
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-02-05 00:00:48 +0100

    finished for now

--- Assets/Contact.cs
+++ Assets/Contact.cs
@@ -1,14 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Contact : MonoBehaviour {
 
 	public GameObject winPanel;
-	
-	private void OnCollisionEnter2D(Collision2D col) {
+
+	private void OnTriggerEnter2D(Collider2D col) {
 		if (!col.gameObject.CompareTag("Plug")) return;
 		col.gameObject.GetComponentInChildren<LineController>().plugged = true;
 		col.gameObject.GetComponentInChildren<LineController>().timer.timerIsRunning = false;
 		winPanel.SetActive(true);
 	}
-	
+
 }
