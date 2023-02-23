9de95a87e20461a9e577657121c618e6ac91404c
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-02-23 19:26:49 +0100

    more progress on game

--- Assets/Scripts/Contact.cs
+++ Assets/Scripts/Contact.cs
@@ -13,7 +13,7 @@ public class Contact : MonoBehaviour {
 		if (!col.gameObject.CompareTag("Plug")) return;
 		var lineController = col.gameObject.GetComponentInChildren<LineController>();
 		col.gameObject.GetComponent<FixedJoint2D>().connectedBody = _rb;
-		col.gameObject.GetComponent<FixedJoint2D>().connectedAnchor = new Vector2(-0.2f, 0.1f);
+		col.gameObject.GetComponent<FixedJoint2D>().connectedAnchor = new Vector2(0.05f, 0f);
 		lineController.plugged = true;
 		lineController.timer.timerIsRunning = false;
 		Invoke(nameof(ActivateWinPanel), 2f);
